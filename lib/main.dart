88a31279917bb5f31a098dd2c7f5d67dbdfb51f8
Jacoai <grotesquevodevil@gmail.com>
2026-01-18 15:27:49 +0300

    add database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:real_estate_agency/core/database/database.dart';
 import 'package:real_estate_agency/core/navigation/router.dart';
 
-void main() {
+late LocalDatabase database;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  database = LocalDatabase();
   runApp(const MainApp());
 }
 

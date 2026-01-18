f0f5f1ba2ee35bc5afe122e0d6b4c136dc265784
Jacoai <grotesquevodevil@gmail.com>
2026-01-18 18:01:36 +0300

    add adding and deleting landlords

--- lib/feature/landlords/presentation/bloc/landlord_page_event.dart
+++ lib/feature/landlords/presentation/bloc/landlord_page_event.dart
@@ -3,3 +3,21 @@ part of 'landlord_page_bloc.dart';
 sealed class LandlordPageEvent {}
 
 class LandlordPageOpenedEvent extends LandlordPageEvent {}
+
+class AddLandlordEvent extends LandlordPageEvent {
+  final String name;
+  final String phone;
+  final String email;
+  final String? adress;
+  AddLandlordEvent({
+    required this.name,
+    required this.email,
+    required this.phone,
+    this.adress,
+  });
+}
+
+class DeleteLandlordEvent extends LandlordPageEvent {
+  final String id;
+  DeleteLandlordEvent({required this.id});
+}
